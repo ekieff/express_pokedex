706ad6fb57762ca4bf65eabdafbfad4c379a6b55
Elaine <elaine@Felipes-iMac.lan1>
2020-08-10 10:46:50 -0500

    [routes] add notes on id

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -21,11 +21,12 @@ router.post('/', (req, res) => {
 
 
 router.get('/:id', (req, res) => {
-  let pokeID = req.params.id; 
+  let pokeID = req.params.id; //I'm not sure if I understand how this works, but it's what we've been doing so I used it. 
+  //Still trying to read more about it, or maybe I don't need to focus so much on theory.
   let pokemonAPI = 'http://pokeapi.co/api/v2/pokemon/' + pokeID;
   axios.get(pokemonAPI).then(response => {
     let pokemonData = response.data;
-    res.render('show', {pokemon: pokemonData})
+    res.render('show', {pokemon: pokemonData}) //open show view, send pokemonData to produce pokemon
   })
 });
 
