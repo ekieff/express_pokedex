c819e701a63dd11704124276ec2ed562e9e9e125
Elaine <elaine@Felipes-iMac.lan1>
2020-08-10 10:39:20 -0500

    [async] remove extraneous async functions

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,41 +5,28 @@ var axios = require('axios');
 
 //callback function, displays the pokemon posted.
 router.get('/', async (req, res) => {
-  try{
     const favorites = await db.pokemon.findAll(); //identifies data called by favorites
     res.render("pokemon", {pokemon: favorites}) //rendering /pokemon.ejs, defintes pokemon as the data posted to favorites
     //struggled with this syntax the most, need to review this array structure more
-  } catch (err){
-    res.send(err, "Error");
-  }
 });
 //when form post submitted...
-router.post('/', async (req, res) => {
-try {
-  await db.pokemon.findOrCreate({
+router.post('/', (req, res) => {
+  db.pokemon.findOrCreate({
     where: {
       name: req.body.name //create new pokemon in database with the button's name.
     },
   })
   res.redirect('/pokemon') //open up the favorites page upon selection
-
-} catch (err){
-  console.log(err, "Error")
-}
 });
 
 
-router.get('/:id', async (req, res) => {
-try {
+router.get('/:id', (req, res) => {
   let pokeID = req.params.id;
   let pokemonAPI = 'http://pokeapi.co/api/v2/pokemon/' + pokeID;
   axios.get(pokemonAPI).then(response => {
     let pokemonData = response.data;
     res.render('show', {pokemon: pokemonData})
   })
-} catch (err) {
-    res.send(err, "Error")
-  }
 });
 
 module.exports = router;
